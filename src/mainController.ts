0690368d7c139c80784cbbca131692b1d3326005
ko1steve <ko1steveg@gmail.com>
2024-05-08 13:59:38 +0800

    add main controller

---
+++ src/mainController.ts
@@ -0,0 +1,93 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'src/styles.css'
+import { CommandType } from './mainConfig'
+import { TabContentController } from './component/tabContent/tabContentController'
+
+export class MainController {
+  protected tabContentControllerMap: Map<CommandType, TabContentController> = new Map()
+
+  protected currentCommandType: CommandType = CommandType.DML
+
+  constructor () {
+    this.addEventListeners()
+  }
+
+  protected addEventListeners (): void {
+    const fileInputDdl: HTMLInputElement = document.getElementById('fileInput-DDL') as HTMLInputElement
+    if (fileInputDdl != null) {
+      fileInputDdl.onchange = this.onFileInput.bind(this, fileInputDdl)
+    }
+    const fileInputDml: HTMLInputElement = document.getElementById('fileInput-DML') as HTMLInputElement
+    if (fileInputDml != null) {
+      fileInputDml.onchange = this.onFileInput.bind(this, fileInputDml)
+    }
+    const dmlTab = document.getElementById('dml-tab')
+    if (dmlTab != null) {
+      dmlTab.onclick = this.onNavClick.bind(this, CommandType.DML)
+    }
+    const ddlTab = document.getElementById('ddl-tab')
+    if (ddlTab != null) {
+      ddlTab.onclick = this.onNavClick.bind(this, CommandType.DDL)
+    }
+    const downloadButton = document.getElementById('download-button')
+    if (downloadButton != null) {
+      downloadButton.onclick = this.onDownloadClick.bind(this)
+    }
+  }
+
+  protected onFileInput (fileInput: HTMLInputElement): void {
+    if (fileInput?.files?.length === 0) {
+      return
+    }
+    let commandType: CommandType = CommandType.NONE
+    Object.values(CommandType).forEach(e => {
+      if (e.toString() === fileInput.dataset.sqlType) {
+        commandType = e
+      }
+    })
+    if (commandType === CommandType.NONE) {
+      return
+    }
+    const reader: FileReader = new FileReader()
+    reader.onload = (event) => {
+      if (event.target == null) {
+        return
+      }
+      const textFromFileLoaded: string = event.target.result as string
+      if (this.tabContentControllerMap.has(commandType)) {
+        const tabContentController = this.tabContentControllerMap.get(commandType) as TabContentController
+        tabContentController.resetPageContent(textFromFileLoaded)
+      } else {
+        const tabContentController = new TabContentController(commandType, textFromFileLoaded)
+        this.tabContentControllerMap.set(commandType, tabContentController)
+      }
+      fileInput.files = null
+      fileInput.value = ''
+    }
+    if (fileInput.files != null) {
+      reader.readAsText(fileInput.files[0], 'UTF-8')
+    }
+  }
+
+  protected onNavClick (commamdType: CommandType) {
+    this.currentCommandType = commamdType
+    const uplloadButtonContainer = document.getElementsByClassName('upload-button-container')[0]
+
+    const label: HTMLLabelElement = uplloadButtonContainer.getElementsByTagName('label')[0]
+    label.id = label.id.replace(CommandType.DDL, commamdType).replace(CommandType.DML, commamdType)
+    label.htmlFor = label.htmlFor.replace(CommandType.DDL, commamdType).replace(CommandType.DML, commamdType)
+
+    const input: HTMLInputElement = uplloadButtonContainer.getElementsByTagName('input')[0]
+    input.id = input.id.replace(CommandType.DDL, commamdType).replace(CommandType.DML, commamdType)
+    input.dataset.sqlType = commamdType
+  }
+
+  protected onDownloadClick (): void {
+    if (!this.tabContentControllerMap.has(this.currentCommandType)) {
+      return
+    }
+    const tabContentController = this.tabContentControllerMap.get(this.currentCommandType)
+    tabContentController?.downloadXML()
+  }
+}
