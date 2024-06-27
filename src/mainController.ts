64029109d3dabb2f697c8e1768591f3aaf879619
ko1steve <ko1steveg@gmail.com>
2024-06-27 16:06:58 +0800

    update example

--- src/mainController.ts
+++ src/mainController.ts
@@ -143,25 +143,35 @@ export class MainController {
   }
 
   protected onDownloadExampleClick (): void {
-    const xmlContent = '--#PreSQL\r\n' +
-      '/*--!*/\r\n' +
-      '--請放置前置語法\r\n' +
+    const xmlContent = '--#PreSQL' + '\r\n' +
+      '--請放置前置語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'SET CONTEXT_INFO 0x12345678' + '\r\n' +
       '\r\n' +
-      '--#CountSQL\r\n' +
-      '/*--!*/\r\n' +
-      '--請放置Count語法\r\n' +
+      '--#PreProdSQL' + '\r\n' +
+      '--請放置PreProd前置語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'SET CONTEXT_INFO 0x12345678' + '\r\n' +
       '\r\n' +
-      '--#SelectSQL\r\n' +
-      '/*--!*/\r\n' +
-      '--請放置異動前/後語法\r\n' +
+      '--#CountSQL' + '\r\n' +
+      '--請放置Count語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'SELECT COUNT(*) FROM EASY.TEST1' + '\r\n' +
       '\r\n' +
-      '--#MainSQL\r\n' +
-      '/*--!*/\r\n' +
-      '--請放置異動語法\r\n' +
+      '--#SelectSQL' + '\r\n' +
+      '--請放置異動前/後語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'SELECT * FROM EASY.TEST1 WHERE ID=1' + '\r\n' +
       '\r\n' +
-      '--#PostSQL\r\n' +
-      '/*--!*/\r\n' +
-      '--請放置後置語法'
+      '--#MainSQL' + '\r\n' +
+      '--請放置異動語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'INSERT INTO EASY.TEST1(ID,NAME) VALUES(101,Easy)' + '\r\n' +
+      '\r\n' +
+      '--#PostSQL' + '\r\n' +
+      '--請放置後置語法' + '\r\n' +
+      '/*--!*/' + '\r\n' +
+      'SET CONTEXT_INFO 0x0'
 
     const blob = new Blob([xmlContent], { type: 'text/xml' })
     const a = document.createElement('a')
