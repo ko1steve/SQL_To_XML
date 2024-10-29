c9a8a9590224adf9b5174cbc874611180d7a275c
ko1steve <ko1steveg@gmail.com>
2024-10-29 15:13:16 +0800

    add spec files for testing

---
+++ tests/data/stringBuilder.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { StringBuilder } from '../../src/data/stringBuilder';
+
+describe('StringBuilder', () => {
+  const stringBuilder = new StringBuilder()
+  stringBuilder.append('1')
+  stringBuilder.append('2')
+
+  it('.strings', () => {
+    expect(stringBuilder.strings[0]).equal('1');
+    expect(stringBuilder.strings[1]).equal('2');
+  });
+
+  it('.size', () => {
+    expect(stringBuilder.size).equal(2);
+  });
+
+  it('.toString()', () => {
+    expect(stringBuilder.toString()).equal('12');
+    expect(stringBuilder.toString(' ')).equal('1 2');
+    expect(stringBuilder.toString(',')).equal('1,2');
+  });
+});
